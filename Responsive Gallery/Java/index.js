fad0d569d5f9458b5fa476fd5bd38dbde3338ed8
mmillustrates <84076263+mmillustrates@users.noreply.github.com>
2023-10-24 12:00:04 +0100

    Add files via upload

---
+++ Responsive Gallery/Java/index.js
@@ -0,0 +1,7 @@
+// SCROLL TO TOP BUTTON
+let mybutton = document.getElementById("top-bttn");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
